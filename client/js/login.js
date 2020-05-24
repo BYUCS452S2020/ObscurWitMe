d8bd3b17e9b5ccb288f8e4068e3a04b1314ac7f4
Miki Jauhiainen <mikimj97@gmail.com>
2020-05-24 14:31:43 -0600

    Add login

---
+++ client/js/login.js
@@ -0,0 +1,5 @@
+function validate() {
+  username = document.getElementById("username").textContent;
+  password = document.getElementById("password").textContent;
+  alert(username + " " + password)
+}
