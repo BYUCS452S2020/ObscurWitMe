8359a4679f31f912dcd726b48259e7ffa6cde56e
Jared Rydalch <jaredsrydalch@gmail.com>
2020-06-11 00:10:12 -0600

    Fixing get_interest
    Jared is good at coding and never forgets to change copy-pasted code. Yep!

--- mongoserver.py
+++ mongoserver.py
@@ -114,8 +114,8 @@ class MongoServerHandler(BaseHTTPRequestHandler):
         cursor = db.getInterestByID(interestID)
         for row in cursor:
             response['interestid'] = str(row['_id'])
-            response['name'] = row['firstName']
-            response['description'] = row['lastName']
+            response['name'] = row['name']
+            response['description'] = row['description']
             response['imageURL'] = row['imageURL']
             response['categories'] = row['categories']
             response['users'] = row['users']
